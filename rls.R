75f7d079254fb62bf6fc12faaf8f39c72ba441de
Julia <maju.murata@gmail.com>
2022-11-20 16:37:54 +0100

    new subset of rls

--- rls.R
+++ rls.R
@@ -6,20 +6,24 @@
 # load required libraries
 library(tidyverse) # v. 1.3.2
 
+# clear memory
+rm(list=ls())
+
 # set working directory
 setwd("~/Downloads")
 
 # read RLS dataset, skip rows containing metadata (1-71)
-rls_raw = read_delim("RLS_20221117.csv", skip = 71, delim = ",")
+rls_raw = read_delim("/media/mari/Crucial X8/RLS_20190101_20221120.csv", skip = 71, delim = ",")
 
 # subset data by year, note: we might use only surveys from one year
 # str(rls_raw) # date seems to have the correct format...
 # rls_2020 = rls_raw %>% filter(survey_date < "2021-01-01")
-rls_2021 = rls_raw %>% filter(survey_date > "2020-12-31")
+#rls_2021 = rls_raw %>% filter(survey_date > "2020-12-31")
 
-# extract coordinates for survey sites in 2021,
+# extract coordinates for survey sites
 ## we need this to subset the sst & chlA data based on the RLS sites
-survey_coordinates = rls_2021 %>% 
-  distinct(site_code, .keep_all = TRUE) %>%
-  select(latitude, longitude)
-# note: survey_coordinates is a tibble, we might need to tranform this into an array-like format to make it compatible with netCDF
+## subset unique coordinate pairs
+survey_coordinates = rls_raw %>% select(latitude, longitude) %>% distinct
+
+## write coordinates in csv
+write.csv(survey_coordinates,"/media/mari/Crucial X8/survey_coordinates.csv", row.names = FALSE) # comma as sep.
