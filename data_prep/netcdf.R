282b2007a0b3c9bc7edc9b52257830ebb034e14f
Julia <maju.murata@gmail.com>
2022-11-23 11:19:59 +0100

    sst data 2018

--- data_prep/netcdf.R
+++ data_prep/netcdf.R
@@ -1,39 +1,84 @@
-# QUANTIFICATION OF ENVIRONMENTAL PREDICTABILITY ACCORDING TO BURGESS & MARSHALL (2015)
+# EXPEDITION INTO THE WORLD OF netCDF
 ## here we have 3-dimensional data: latitude, longitude, time
+## the dataset used in the little test run contains gridded daily mean sst measurements from:
+## the steps are derived from: https://towardsdatascience.com/how-to-crack-open-netcdf-files-in-r-and-extract-data-as-time-series-24107b70dcd
 
 # load required libraries
+#library(forecast)
 library(ncdf4)
-library(raster)
-library(rerddap)
+#library(raster)
+#library(rerddap)
 library(tidyverse)
+#library(envPred)
 
-# clear memory
+# we like a clean environment: clear memory
 rm(list=ls())
 
 # set working directory
 setwd("~/Downloads")
 
-# read netCDF file
-sst_2022 = nc_open("subset.nc")
+# open the netCDF file, here we only look at sst measurements from 2018
+sst_2018 = nc_open("subset.nc")
+print(sst_2018)
 
-# data exploration
-dim(sst_2022)
-print(sst_2022)
+##### EXTRACT RELEVANT DATA #####
 
-# get variables
-time = ncvar_get(sst_2022, "time")
-lat = ncvar_get(sst_2022, "lat")
-lon  = ncvar_get(sst_2022, "lon")
+# print names of variable (SST) and dimensions (time, longitude, latitude)
+attributes(sst_2018$var) # sst
+attributes(sst_2018$dim) # t, lon, lat
 
-# check dimensions of lat and long
+# write lats and lons into objects
+## latitude
+lat = ncvar_get(sst_2018, "lat")
 nlat = dim(lat)
+## longitude
+lon  = ncvar_get(sst_2018, "lon")
 nlon = dim(lon)
-print(c(nlat, nlon))
+## compare spatial dimensions with metadata, i.e., print(sst_2018)
+print(c(nlon, nlat)) # output: [1] 720 350, alrighty!
 
-# get temperature
-dname = "sst"
-tmp_array <- ncvar_get(sst_2022,dname)
-dlname <- ncatt_get(sst_2022,dname,"long_name")
-dunits <- ncatt_get(sst_2022,dname,"units")
-fillvalue <- ncatt_get(sst_2022,dname,"_FillValue")
-dim(tmp_array)
+## get the time dimension
+## write time into an object
+time = ncvar_get(sst_2018, "time")
+head(time) # days are stored as in numerical order
+ntime = dim(time) # It's 365, good.
+## get units for time (days)
+tunits <- ncatt_get(sst_2018, "time", "units")
+
+# sea surface temperature data
+## write sst into an object, i.e., array
+sst_array = ncvar_get(sst_2018, "sst")
+## get fill values
+fillvalue = ncatt_get(sst_2018, "sst", "_FillValue")
+## check dimensions
+dim(sst_array) # output: [1] 720 350 365, i.e., lat long time
+## replace fill values by NAs
+sst_array[sst_array == fillvalue$value] <- NA
+
+
+##### TRANSFORM DATA INTO HUMAN-FRIENDLY FORMAT #####
+
+# convert time units into YYYY-MM-DD format
+time_obs = as.POSIXct.Date(time,
+                           origin = "2018–01–01",
+                           tz ="GMT")
+dim(time_obs) # output: [1] 365
+range(time_obs) # check start and end date
+
+# create a dataframe with all netCDF variables
+## write sst values of the whole area at day 50 
+sst_slice <- sst_array[ , , 50]
+## plot sst at day 50
+image(lon, lat, sst_slice)
+## 2D-matrix with lat, lon, time
+dim_mx = as.matrix(expand.grid(lon, lat, time_obs))
+## write sst array into vector
+sst_vec_long <- as.vector(sst_array)
+length(sst_vec_long) 
+# merge all into dataframe
+sst_df <- data.frame(cbind(dim_mx, sst_vec_long))
+
+
+
+
+
