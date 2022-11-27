3d8e2d451d6dfd9df07facf7600d14e6ac1a7e6c
Julia <maju.murata@gmail.com>
2022-11-27 18:32:54 +0100

    subset rls data

--- data_prep/rls_site_coordinates.R
+++ data_prep/rls_site_coordinates.R
@@ -14,13 +14,20 @@ setwd("~/Downloads")
 
 # read RLS dataset, skip rows containing metadata (1-71)
 rls_raw = read_delim("/media/mari/Crucial X8/RLS_20190101_20221120.csv", skip = 71, delim = ",")
+rls_2021_2022 = read_delim("/media/mari/Crucial X8/RLS_2021_2022.csv", skip = 71, delim = ",")
 
 # extract coordinates for survey sites
 ## we need this to subset the sst & chlA data based on the RLS sites
 ## subset unique coordinate pairs
-survey_coordinates = rls_raw %>%
+survey_coordinates = rls_raw %>% 
+  select(latitude, longitude) %>%
+  distinct
+
+# for 2021 and 2022 only
+survey_coordinates_2021_2022 = rls_2021_2022 %>% 
   select(latitude, longitude) %>%
   distinct
 
 ## write coordinates in csv
 write.csv(survey_coordinates,"/media/mari/Crucial X8/survey_coordinates.csv", row.names = FALSE) # comma as sep.
+write.csv(survey_coordinates_2021_2022,"/media/mari/Crucial X8/survey_coordinates_2021_2022.csv", row.names = FALSE) # comma as sep.
