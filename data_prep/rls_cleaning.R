792bc283499f8dafc2f9949a1082d12c4ea5dc65
Julia <maju.murata@gmail.com>
2022-12-04 14:01:40 +0100

    several stupid things

---
+++ data_prep/rls_cleaning.R
@@ -0,0 +1,37 @@
+# CLEAN-UP OF RLS DATA
+## average abundance data over depth and block, we want only one survey per time and locations
+
+# load libraries
+library(tidyverse)
+
+# clean memory
+rm(list=ls())
+
+# set working directory
+setwd("~/projects/msc_thesis")
+
+# read RLS dataset
+rls_2021_2022 = read_delim("/media/mari/Crucial X8/RLS_2021_2022.csv", skip = 71, delim = ",")
+
+# select relevant columns
+rls_sub = rls_2021_2022 %>%
+  select(
+    latitude, longitude, survey_date, 
+    class, order, family, species_name, 
+    size_class, total, biomass
+  )
+
+# average size_class, total, biomass
+rls_avg = rls_sub %>% 
+  group_by(latitude, longitude, survey_date, species_name) %>% 
+  summarise(
+    size_class_mean = round(mean(size_class), digits = 1),
+    total_mean = round(mean(total), digits = 1),
+    biomass_mean = round(mean(biomass), digits = 1)
+    ) %>% 
+  arrange(survey_date) %>% 
+  ungroup()
+
+# write into csv
+write.csv(rls_avg,"~/projects/msc_thesis/data/rls_2021_2022_avg.csv", row.names = FALSE)
+write.csv(rls_avg,"/media/mari/Crucial X8/rls_2021_2022_avg.csv", row.names = FALSE)
--- env_predictability/envpred.R
+++ env_predictability/envpred.R
@@ -1,4 +1,4 @@
-# ENVIRONMENTAL PREDICTABILITY
+# ENVIRONMENTAL PREDICTABILITY FOR SST
 ## envPred package by dbarneche (https://github.com/dbarneche)
 ## installation: library(devtools), install_github("dbarneche/envPred")
 
@@ -12,96 +12,53 @@ rm(list=ls())
 # set working directory
 setwd("~/projects/msc_thesis")
 
-# # test envPred package with dummy data
-# ## create dummy data to test the package
-# set.seed(400)
-# dummy_sst = runif(n=365*10+3, min=3, max=20)
-# date = seq(as.Date("2013-1-1"), as.Date("2023-1-1"), by = "days")
-# 
-# ## calculate stats delivered by the envpred package
-# env_stats(time_series = dummy_sst,
-#           date = date,
-#           delta = 1,
-#           noise_method = 'spectrum'
-#           )
+# test envPred package with one time series
+## read file
+test_csv = read_delim("/media/mari/Crucial X8/sst_csv/-14.10_123.55.csv", delim = ",")
+## select column with temp measurements and a time span of 11 years
+test_csv = test_csv %>%
+  select(date, analysed_sst) %>%
+  dplyr::filter(date >= "2009-01-01" & date <= "2020-01-01")
+## read names of the statistics into variable (we need this later...)
+env_stats_cols = names(env_stats(
+  time_series = test_csv$analysed_sst,
+  dates = test_csv$date,
+  delta = 1,
+  noise_method = 'spectrum'
+))
 
-# I have a folder with one time series per site
-# read the file with converting filenames into variable names
-# apply the following function relative to the survey time in rls_site_depth_ID.csv
-# calculate envPred statistics -12.24_122.98.csv
+# read RLS dataset
+rls_avg = read_delim("/media/mari/Crucial X8/rls_2021_2022_avg.csv", delim = ",")
 
-# list files in vector
-list_csv_files <- list.files(path = "/media/mari/Crucial X8/sst_csv/", pattern="*.csv")
-
-# remove .csv from filenames
-file_names = substring(list_csv_files, 1, nchar(list_csv_files)-4)
-
-test_csv = read.table("/media/mari/Crucial X8/sst_csv/-11.98_123.38.csv", quote="\"", fill = TRUE)
-
-rls_2021_2022 = read_delim("/media/mari/Crucial X8/RLS_2021_2022.csv", skip = 71, delim = ",")
-
-
-# 
-# average over depth grouped by survey date
-# unique coordinate AND survey date
-# stringr, write coords from string in columns
-
-# distinct lat, long, survey date
-rls_summary = rls_2021_2022 %>% 
+# select distinct combinations of lat, long, AND survey date
+rls_unique = rls_avg %>% 
   select(longitude, latitude, survey_date) %>% 
   distinct()
 
-rls_summary$sst_filename = paste(rls_summary$latitude, "_", rls_summary$longitude, ".csv", sep = "")
-list_csv_files
+# write new column with latitude, longitude, and .csv
+rls_unique$sst_filename = paste(rls_unique$latitude, "_", rls_unique$longitude, ".csv", sep = "")
+#list_csv_files
 
-rls_summary$sst_mean = NA
-
-for (i in 1:nrow(rls_summary)){
-  temp_filename = paste("/media/mari/Crucial X8/sst_csv/", as.character(rls_summary[i,4]), sep = "")
-  if(file.exists(temp_filename)){
-    print(i)
-    
-    temp_sst_data = read_delim(temp_filename, delim = ',')
-    print(mean(temp_sst_data$analysed_sst))
-    
-    # rls_summary$sst_mean[i] = mean(temp_sst_data$analysed_sst)
-  }
+# create "empty" columns with NAs, one for each env_stats variable
+for (statname in env_stats_cols) {
+  rls_unique[statname] = NA
 }
 
-
-
-
-
-
-
-# read data from github
-rls_unique = read_delim("/media/mari/Crucial X8/rls_site_depth_ID.csv", delim = ",")
-rls_coords = read_delim("/media/mari/Crucial X8/survey_coordinates_2021_2022.csv", skip = 71, delim = ",")
-
-# write filenames into vector
-# raw_files <- tibble(filename = list.files('/media/mari/Crucial X8/sst_csv/'))
-
-# rename column names
-colnames(test_csv) <- c('dates','value','variable')
-
-# write each variable into one column
-test_csv_wide = test_csv %>% 
-  distinct() %>% 
-  pivot_wider(names_from = variable,
-              values_from = value
-              )
-
-# change format of dates column
-test_csv_wide$dates = as.Date(test_csv_wide$dates, "%Y/%m/%d")
-
-# select sst and subset a period of 10 years between 2010 and 2020
-test_ts = test_csv_wide %>%
-  select(dates, analysed_sst) %>% 
-  dplyr::filter(dates > "2009-12-31" & dates < "2021-01-02")
-
-# envPred
-env_stats = env_stats(time_series = test_ts$analysed_sst,
-                      dates = test_ts$dates,
-                      delta = 1,
-                      noise_method = 'spectrum'
-                      )
+#
+for (i in 1:nrow(rls_unique)) {
+  temp_filename = paste("/media/mari/Crucial X8/sst_csv/", as.character(rls_unique[i,4]), sep = "")
+  
+  if (!file.exists(temp_filename)) {
+    print(paste(temp_filename, "not found", sep = " "))
+    next
+  }
+  
+  temp_sst_data = read_delim(temp_filename, delim = ',') # read respective file
+  start_date = rls_unique[i, "survey_date"] - as.difftime(11, unit = "years") # calculate time range relative to survey date
+  pred_range = temp_sst_data[date >= start_date & date <= rls_unique[i, "survey_date"]] # subset time series data based on relative time range
+  envpred_statistics = env_stats(pred_range$analysed_sst) # write environmental statistics into df
+  
+  for (statname in env_stats_cols) { # write each statistic into a new column in rls_unique
+    rls_unique[statname] = envpred_statistics[statname]
+  }
+}
