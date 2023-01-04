962b40ea3108d4a382b44412e0d88bd7a920e55a
Julia <maju.murata@gmail.com>
2023-01-04 12:40:46 +0100

    averaged data updated

--- life_history_traits/rls_averaging.R
+++ life_history_traits/rls_averaging.R
@@ -1,5 +1,5 @@
-# CLEAN-UP OF RLS DATA
-## average abundance data over depth and block, we want only one survey per time and locations
+# RLS DATA
+## average abundance data of each species over depth and block per survey
 
 # load libraries
 library(tidyverse)
@@ -13,13 +13,22 @@ setwd("~/projects/msc_thesis")
 # read RLS dataset
 rls_2021_2022 = read_delim("/media/mari/Crucial X8/RLS_2021_2022.csv", skip = 71, delim = ",")
 
-## remove brackets from species name
+# data exploration
+# n_distinct(rls_2021_2022$survey_id) # 1276, identifier for each survey, note that we don't differ between depths
+# n_distinct(rls_2021_2022$site_code) # 475, identifier for each location
+# n_distinct(rls_2021_2022$FID) # 91410, identifier for each line
+# # unique(rls_2021_2022$ecoregion)
+# nrow(unique(rls_2021_2022[c('latitude', 'longitude', 'survey_date')])) # 490 observations
+
+## remove brackets from species name, e.g., change Pomacentrus sp. [rhodonotus] to Pomacentrus rhodonotus
+## write function that explaces the different signs
 rmBrackets = function(spname){
   return(str_replace_all(spname, "sp\\. \\[([^\\\\]*)\\]", "\\1"))
 }
+## apply function on species column in original file
 rls_2021_2022["species_name"] <- lapply(rls_2021_2022["species_name"], rmBrackets)
 
-# select relevant columns
+# subset data 
 rls_sub = rls_2021_2022 %>%
   select(
     latitude, longitude, survey_date, 
@@ -27,7 +36,7 @@ rls_sub = rls_2021_2022 %>%
     size_class, total, biomass
   )
 
-# average size_class, total, biomass
+# average size_class, total, biomass for each species per survey, i.e., unique combinations of latitude, longitude, and survey_date
 rls_avg = rls_sub %>% 
   group_by(latitude, longitude, survey_date, species_name) %>% 
   summarise(
@@ -35,8 +44,16 @@ rls_avg = rls_sub %>%
     total_mean = round(mean(total), digits = 1),
     biomass_mean = round(mean(biomass), digits = 1)
     ) %>% 
-  arrange(survey_date) %>% 
+  mutate(ID = cur_group_id()) %>% # assign unique id number to each group
   ungroup()
+
+# extract taxonomic information for each species from original file
+taxonomy = rls_2021_2022 %>% 
+  select(class, order, family, species_name) %>% 
+  distinct(species_name, .keep_all = TRUE)
+
+# add class, order, family to averaged rls dataframe
+rls_avg = left_join(rls_avg, taxonomy, by = "species_name")
 
 # write into csv
 write.csv(rls_avg,"~/projects/msc_thesis/data/rls_2021_2022_avg.csv", row.names = FALSE)
