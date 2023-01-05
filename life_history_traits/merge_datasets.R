d004be92184d1daf8367745afa1bae8863724fe8
Julia <maju.murata@gmail.com>
2023-01-05 15:21:35 +0100

    digits, community columns removed

--- life_history_traits/merge_datasets.R
+++ life_history_traits/merge_datasets.R
@@ -1,5 +1,5 @@
 # COMBINE DATA FROM RLS (ABUNDANCE), LUIZ ET AL. (PLD), AND BARNECHE ET AL. (EGGSIZE)
-
+######### test script
 # read libs
 #library(rfishbase)
 library(tidyverse)
@@ -23,48 +23,60 @@ eggsize_raw = read.csv("https://raw.githubusercontent.com/dbarneche/fishEggSize/
 ## separate species name in rls data
 rls_avg_sep = rls_avg %>% 
   separate(species_name, c("Genus", "Species"), extra = "merge", fill = "left")
+## backup dataset if loop kills it later
+backup_rls = rls_avg_sep
 
 # PLD DATA
 pld_subset = pld_raw %>% 
-  select(-c("Region", "Reference")) %>% 
-  mutate(Ref_pld = rep("Luiz2013", length(pld_raw$Family)))
+  select(-c("Family", "Region", "Multi-Habitat", "Transpacific", "Reference")) %>% 
+  mutate(ref_pld = rep("Luiz2013", length(pld_raw$Genus)),
+         ref_bodysize = rep("Luiz2013", length(pld_raw$Genus))) %>% 
+  rename(RangeSize = `Range size (km)`,
+         BodySize = `Body size (cm)`,
+         PLD = `Mean PLD (days)`,
+         DepthRange = `Depth range (m)`,
+         spawningMode = `Spawn`,
+         nocturnal = `Nocturnal`)
+
+## replace spawning classes by strings used in the eggsize dataset
+pld_subset$spawningMode = gsub('PEL', 'pelagic', pld_subset$spawningMode)
+pld_subset$spawningMode = gsub('DEM', 'demersal', pld_subset$spawningMode)
 
 # PLD ALZATE et al. DATA
 pld_alz_subset = pld_alz_raw %>% 
-  select(Species, PLD, Egg, BodySize) %>%
+  select(Species, nocturnal, PLD, Egg, BodySize) %>%
   separate(Species, c("Genus", "Species"), extra = "merge", fill = "left") %>% 
-  mutate(Ref_pld = rep("Alzate2018", length(pld_alz_raw$Family))) %>% 
-  mutate(Ref_bodysize = rep("Alzate2018", length(pld_alz_raw$Family)))
+  mutate(ref_pld = rep("Alzate2018", length(pld_alz_raw$Genus)),
+         ref_bodysize = rep("Alzate2018", length(pld_alz_raw$Genus)))
+
+## replace nocturnal logicals by strings used in the other pld dataset
+pld_alz_subset$nocturnal = gsub('yes', 'YES', pld_alz_subset$nocturnal)
+pld_alz_subset$nocturnal = gsub('no', 'NO', pld_alz_subset$nocturnal)
 
 # FISHBASE DATA
 fb_bodysize = fb_raw %>% 
-  separate(species_name, c("Genus", "Species"), extra = "merge", fill = "left")
+  separate(species_name, c("Genus", "Species"), extra = "merge", fill = "left") %>% 
+  rename(BodySize = Length) %>% 
+  rename(ref_bodysize = Ref_bodysize)
 
 # EGGSIZE DATA
 eggsize_subset = eggsize_raw %>% 
   select(Species, spawningMode, FemaleSize_mm, eggSize_mm) %>% 
   separate(Species, c("Genus", "Species"), extra = "merge", fill = "left") %>% 
   group_by(Genus, Species) %>% 
-  summarise(mean_eggsize = mean(eggSize_mm)) %>% 
+  summarise(eggsize_mean = mean(eggSize_mm)) %>% 
   ungroup() %>% 
-  mutate(Ref_eggsize = rep("Barneche2018", length(mean_eggsize)))
+  mutate(ref_eggsize = rep("Barneche2018", length(eggsize_mean)))
 
 # merge all datasets
-df_list <- list(rls_avg_sep, pld_subset, eggsize_subset)      
+df_list <- list(rls_avg_sep, pld_subset, eggsize_subset, pld_alz_subset[c("Genus", "Species", "Egg")])      
 trait_data = df_list %>% 
   reduce(left_join, by = c("Genus", "Species"))
 
-# rename column names in trait data
-trait_data = trait_data %>% 
-  rename(RangeSize = `Range size (km)`,
-         BodySize = `Body size (cm)`,
-         PLD = `Mean PLD (days)`,
-         DepthRange = `Depth range (m)`)
-
+# complement pld and bodysize data from Alzate
 # replace pld NAs with data from pld,
 # replace BodySize NAs with bodysize from pld,
 # swap refs accordingly
-x <- trait_data
 
 for (i in 1:nrow(trait_data)) {
   if (is.na(trait_data[i, "PLD"]) | is.na(trait_data[i, "BodySize"])) {
@@ -76,39 +88,76 @@ for (i in 1:nrow(trait_data)) {
     
     if (is.na(trait_data[i, "PLD"]) & nrow(pld_rows) > 0 & !is.na(pld_rows[1, "PLD"])) {
       trait_data[i, "PLD"] = pld_rows[1, "PLD"]
-      trait_data[i, "Ref_pld"] = pld_rows[1, "Ref_pld"]
+      trait_data[i, "ref_pld"] = pld_rows[1, "ref_pld"]
     }
     
     if (is.na(trait_data[i, "BodySize"]) & nrow(pld_rows) > 0 & !is.na(pld_rows[1, "BodySize"])) {
       trait_data[i, "BodySize"] = pld_rows[1, "BodySize"]
-      trait_data[i, "Ref_bodysize"] = pld_rows[1, "Ref_bodysize"]
+      trait_data[i, "ref_bodysize"] = pld_rows[1, "ref_bodysize"]
+    }
+    
+    if (is.na(trait_data[i, "nocturnal"]) & nrow(pld_rows) > 0 & !is.na(pld_rows[1, "nocturnal"])) {
+      trait_data[i, "nocturnal"] = pld_rows[1, "nocturnal"]
     }
   }
 }
 
-# # hehe let's see how many complete observations we have...
-# length(unique(rls_avg$species_name)) # 1136
-# nrow(na.omit(trait_data)) # ... well, well..
-# sum(!is.na(trait_data$eggSize_mm))
+# make copy of df
+trait_data_2 = trait_data
 
-# summary(trait_data %>%
-#   select(Genus, Species, eggSize_mm, `Mean PLD (days)`) %>% 
-#   distinct(Genus, Species, .keep_all = TRUE))
-#   
-  
+# complement bodysize data from fishbase
+for (i in 1:nrow(trait_data_2)) {
+  if (is.na(trait_data_2[i, "BodySize"])) {
+    curr_genus = trait_data_2[[i, "Genus"]]
+    curr_species = trait_data_2[[i, "Species"]]
+    bodysize_rows = subset(fb_bodysize,
+                           fb_bodysize[, "Genus"] == curr_genus &
+                             fb_bodysize[, "Species"] == curr_species)
+    
+    if (is.na(trait_data_2[i, "BodySize"]) & nrow(bodysize_rows) > 0 & !is.na(bodysize_rows[1, "BodySize"])) {
+      trait_data_2[i, "BodySize"] = bodysize_rows[1, "BodySize"]
+      trait_data_2[i, "ref_bodysize"] = bodysize_rows[1, "ref_bodysize"]
+    }
+  }
+}
 
-# Data sources:
-## RLS:
-## Reef Life Survey (2022). Public Website, www.reeflifesurvey.com
+# clean data
+## rename column names
+trait_data_2 = trait_data_2 %>% 
+  rename(
+    genus = Genus,
+    species = Species,
+    rangeSize = RangeSize,
+    bodySize = BodySize,
+    depthRange = DepthRange,
+    schooler = Schooler,
+    egg = Egg
+  )
 
-## PLD: 
-## Luiz, O. J., Allen, A. P., Robertsson, D. R. & Madin, J. S. (2013). 
-## Adult and larval traits as determinants of geographic range size among tropical reef fishes. 
-## Proceedings of the National Academy of Sciences of the United States of America, 110(41), 16498-16502. 
-## https://doi.org/10.1073/pnas.1304074110
+## change order of columns
+final_trait_data = trait_data_2[,c(
+  "ID", "latitude", "longitude", "survey_date",
+  "class", "order", "family", "genus", "species",
+  "biomass_mean", "size_class_mean", "total_mean",
+  "bodySize", "PLD", "rangeSize", "depthRange", "eggsize_mean", "egg",
+  "spawningMode", "schooler", "nocturnal",
+  "ref_bodysize", "ref_pld", "ref_eggsize"      
+)]
 
-## eggsize: 
-## Barneche, D. R., Burgess, S. C. & Marshall, D. J. (2018). 
-## Global environmental drivers of marine fish egg size. 
-## Global Ecology and Biogeography, 27(8), 890-898. 
-## https://doi.org/10.1111/geb.12748
+# round values
+final_trait_data <- data.frame(lapply(final_trait_data, function(x) if(is.numeric(x)) round(x, 1) else x))
+
+# subset data with unique species
+unique_species = final_trait_data %>% 
+  select(-c("ID", "latitude", "longitude", "survey_date", "class", "order", "biomass_mean", "size_class_mean", "total_mean")) %>% 
+  distinct(genus, species, .keep_all = TRUE) %>% 
+  arrange(family, genus, species)
+
+# write data
+## full dataset with traits and survey sites
+write.csv(final_trait_data,"~/projects/msc_thesis/data/species_traits_per_survey.csv", row.names = FALSE)
+write.csv(final_trait_data,"/media/mari/Crucial X8/species_traits_per_survey.csv", row.names = FALSE)
+
+## only species and trait data
+write.csv(unique_species,"~/projects/msc_thesis/data/species_traits.csv", row.names = FALSE)
+write.csv(unique_species,"/media/mari/Crucial X8/species_traits.csv", row.names = FALSE)
