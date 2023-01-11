b6c7c2a9bd941f382bc28fc5f839892704872775
Julia <maju.murata@gmail.com>
2023-01-11 17:08:38 +0100

    final dataset, etc

--- life_history_traits/cwm.R
+++ life_history_traits/cwm.R
@@ -17,33 +17,48 @@ species_traits = read_delim("/media/mari/Crucial X8/species_traits_imputed.csv",
 # prepare site data
 ## select columns of survey data
 rls_avg_subset = rls_avg %>% 
-  dplyr::filter(class != "Elasmobranchii") %>% 
+  dplyr::filter(class == "Actinopterygii") %>% 
   select(latitude, longitude, survey_date, species_name, biomass_mean, total_mean)
+
+nrow(rls_avg_subset 
+     %>% select(latitude, longitude, survey_date) 
+     %>% distinct()
+     ) # despite removing all rows with elasmobranchs, we have 490 survey sites
 
 # prepare trait data
 ## copdy df
 species_traits_copy = species_traits
 
-## merge genus and species
+## merge genus and species (as in the initial RLS dataset)
 species_traits_copy$species_name = paste(species_traits_copy$genus, species_traits_copy$species, sep = " ")
 
 ## select columns of trait data
 species_traits_subset = species_traits_copy %>% 
   select(species_name, bodySize, PLD)
 
-## merge datasets
-complete_trait_data = rls_avg_subset %>% 
+# merge datasets
+rls_trait_data = rls_avg_subset %>% 
   left_join(species_traits_subset, by = c("species_name"))
 
-## copy df
-cwm_input = complete_trait_data
+# data check-up, total cases: 20633
+nrow(rls_trait_data[complete.cases(rls_trait_data),]) # 20260 complete cases
+nrow(rls_trait_data[complete.cases(rls_trait_data$biomass_mean),]) # 20260 complete cases with biomass
+nrow(rls_trait_data[complete.cases(rls_trait_data$total_mean),]) # 20633 complete cases with total counts
+nrow(rls_trait_data[complete.cases(rls_trait_data$bodySize),]) # 20633 complete cases with bodysize
+nrow(rls_trait_data[complete.cases(rls_trait_data$PLD),]) # 20633 complete cases with PLD
+
+## copy merged dataset
+cwm_input = rls_trait_data
+
+## check if biomass and total counts are complete
+nrow(cwm_input[complete.cases(cwm_input$biomass_mean),]) # 20260, incomplete
+nrow(cwm_input[complete.cases(cwm_input$total_mean),]) # 20633, complete
+## --> use total observations for weighted means
 
 # caluclate cwms
 trait_cwm = cwm_input %>%
   group_by(latitude, longitude, survey_date) %>%   # Groups the summary file by Plot number
   summarise(           # Coding for how we want our CWMs summarized
-    bodysize_cwm_biomass = weighted.mean(bodySize, biomass_mean),   # Actual calculation of CWMs
-    PLD_cwm_biomass = weighted.mean(PLD, biomass_mean),
     bodysize_cwm_total = weighted.mean(bodySize, total_mean),
     PLD_cwm_total = weighted.mean(PLD, total_mean),
     total_biomass = sum(na.omit(biomass_mean)),
@@ -55,8 +70,7 @@ trait_cwm = cwm_input %>%
   ungroup()
 
 # check if data is missing
-data_with_na = trait_cwm[!complete.cases(trait_cwm),]
-# rls_avg_subset[,!is.na("total_mean")] --> we have missing biomass data, but total counts are complete, so rather use those?
+nrow(trait_cwm[!complete.cases(trait_cwm),]) # nice, no missing data
 
 # round numeric values, but this needs to be checked, maybe it's redundant
 trait_cwm = data.frame(lapply(trait_cwm, function(x) if(is.numeric(x)) round(x, 2) else x))
--- stats/stats_check.R
+++ stats/stats_check.R
@@ -10,54 +10,75 @@ rm(list=ls())
 # set working directory
 setwd("~/Documents/MSc_thesis")
 
+# conditional executions
+save_completed_dataset = FALSE
+
 # import data
 eco_data = read_delim("/media/mari/Crucial X8/cwm_data.csv", delim = ",")
 sst_data = read_delim("/media/mari/Crucial X8/env_stats_sst.csv", delim = ",")
+chla_data = read_delim("/media/mari/Crucial X8/env_stats_chla.csv", delim = ",")
+
+
+###### data preparation
 
 # merge data for analysis
-eco_sst = full_join(eco_data, sst_data, by = c("latitude", "longitude", "survey_date"))
+eco_env = full_join(eco_data, sst_data, by = c("latitude", "longitude", "survey_date"))
+eco_env = full_join(eco_env, chla_data, by = c("latitude", "longitude", "survey_date"))
 ## baaaaaam perfect match
+# double-check if we have incomplete cases...
+nrow(eco_env[complete.cases(eco_env),]) # 416
+nrow(eco_env[complete.cases(eco_env$chla_raw_mean),]) # 416
+## chla is missing for some sites 
+
+# check date of sites that have chla values
+years_with_chla = eco_env %>% 
+  filter(!is.na(chla_raw_mean)) %>% 
+  select(survey_date) %>% 
+  arrange(desc(survey_date))
+### that's weird, there shouldn't be sites with date > 31-07-2021...
+
 # arrange by
-eco_sst = eco_sst %>% 
+eco_env_copy = eco_env %>% 
   arrange(survey_date, latitude, longitude)
 
 # create unique survey id
 ## take row number as numbering
-eco_sst = eco_sst %>% mutate(new_survey_id = row_number())
+eco_env_copy = eco_env_copy %>%
+  mutate(new_survey_id = row_number())
 ## transform number into character
-eco_sst$new_survey_id = as.character(eco_sst$new_survey_id)
+eco_env_copy$new_survey_id = as.character(eco_env_copy$new_survey_id)
 ## add s for survey to each element
-eco_sst$new_survey_id = paste("S", eco_sst$new_survey_id, sep="")
+eco_env_copy$new_survey_id = paste("S", eco_env_copy$new_survey_id, sep="")
+## move column with survey name to start
+eco_env_copy = eco_env_copy %>%
+  select(new_survey_id, everything())
+
+# save dataset
+if (save_completed_dataset == TRUE) {
+  write.csv(eco_env_copy,"~/projects/msc_thesis/data/survey_cwm_envpred_data.csv", row.names = FALSE)
+  write.csv(eco_env_copy,"/media/mari/Crucial X8/survey_cwm_envpred_data.csv", row.names = FALSE)
+} else {
+  print("Protego!")
+}
+
+###### data visualisation
+
+# get variable names
+names(eco_env_copy)
 
 # quick correlation check
-## with a formula
-pairs(~ bodysize_cwm_biomass + PLD_cwm_biomass + sst_bounded_seasonality + sst_env_col, data = eco_sst)
-## remove survey data
-var_data = eco_sst %>% 
-  select(-c("latitude", "longitude", "survey_date", "new_survey_id",
-            "bodysize_cwm_biomass", "PLD_cwm_biomass",
-            "inv_simpson", "sst_raw_mean", "sst_raw_var", "sst_raw_cv",
-            "sst_predicted_var", "sst_unpredicted_var", "sst_unbounded_seasonality",
-            "sst_colwell_c", "sst_colwell_m", "sst_colwell_p"))
+pairs(~ sst_env_col + sst_raw_mean + sst_bounded_seasonality + bodysize_cwm_total + inv_simpson, data = eco_env_copy)
 
-# Plot correlation matrix
-pairs(var_data)
-# corrplot(var_data, method = "number")
+# remove outliers from "sst_bounded_seasonality"
+## calculate quantiles
+Q1 <- quantile(eco_env_copy$sst_bounded_seasonality, .25)
+Q3 <- quantile(eco_env_copy$sst_bounded_seasonality, .75)
+IQR <- IQR(eco_env_copy$sst_bounded_seasonality)
 
-Q1 <- quantile(eco_sst$sst_bounded_seasonality, .25)
-Q3 <- quantile(eco_sst$sst_bounded_seasonality, .75)
-IQR <- IQR(eco_sst$sst_bounded_seasonality)
+## only keep rows in dataframe that have values within 1.5*IQR of Q1 and Q3
+no_outliers <- subset(eco_env_copy, eco_env_copy$sst_bounded_seasonality> (Q1 - 1.5*IQR) & 
+                        eco_env_copy$sst_bounded_seasonality< (Q3 + 1.5*IQR))
 
-#only keep rows in dataframe that have values within 1.5*IQR of Q1 and Q3
-no_outliers <- subset(eco_sst, eco_sst$sst_bounded_seasonality> (Q1 - 1.5*IQR) & 
-                        eco_sst$sst_bounded_seasonality< (Q3 + 1.5*IQR))
+# correlation check without outliers from "sst_bounded_seasonality"
+pairs(~ sst_env_col + sst_raw_mean + sst_bounded_seasonality + bodysize_cwm_total + inv_simpson, data = no_outliers)
 
-var_data_2 = no_outliers %>% 
-  select(-c("latitude", "longitude", "survey_date", "new_survey_id",
-            "bodysize_cwm_biomass", "PLD_cwm_biomass",
-            "inv_simpson", "sst_raw_mean", "sst_raw_var", "sst_raw_cv",
-            "sst_predicted_var", "sst_unpredicted_var", "sst_unbounded_seasonality",
-            "sst_colwell_c", "sst_colwell_m", "sst_colwell_p"))
-
-# Plot correlation matrix
-pairs(var_data_2)
