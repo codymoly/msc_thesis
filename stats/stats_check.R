0c9b84dc76d6fdd2fde224bd15963e4f8c5207c2
Julia <maju.murata@gmail.com>
2023-02-08 16:47:45 +0100

    dredged

--- stats/stats_check.R
+++ stats/stats_check.R
@@ -5,6 +5,8 @@ library(tidyverse)
 library(grid)
 library(gridExtra)
 library(ggpubr)
+library(broom)
+library(corrplot)
 
 # clean memory
 rm(list=ls())
@@ -83,8 +85,9 @@ plot(binned_subset$longitude, binned_subset$latitude)
 ###### know your data
 
 # distribution
-summary(binned_subset)
-plot(density(binned_subset$number_total))
+summary(binned_subset_2)
+qqp(binned_subset_2$sst_raw_mean)
+qqp(binned_subset_2$sst_raw_mean)
 ### skewed variables: number_total, bodysize_cwm_total, bodysize_cwv_total, total_biomass,
 ### sst_raw_mean, sst_raw_var, sst_bounded_seasonality, sst_colwell_p
 
@@ -126,6 +129,13 @@ IQR <- IQR(binned_subset_2$total_biomass, na.rm = TRUE)
 binned_subset_2$total_biomass[binned_subset_2$total_biomass > (Q3 + 2*IQR)] = NA
 
 ## plot again
+pairs(~ log(sst_raw_mean) +
+        sst_raw_var +
+        sst_bounded_seasonality +
+        sst_env_col +
+        log(bodysize_cwm_total),
+      data = binned_subset_2) # yepp
+
 pairs(~ sst_raw_mean +
         sst_raw_var +
         sst_bounded_seasonality +
@@ -134,7 +144,7 @@ pairs(~ sst_raw_mean +
         bodysize_cwv_total +
         sp_richness +
         total_biomass,
-      data = binned_subset_2) # yepp
+      data = binned_subset_2)
 
 # spatial distribution of the subsample
 plot(binned_subset_2$longitude, binned_subset$latitude)
@@ -196,38 +206,168 @@ annotate_figure(bio_plot,
                                 color = "black", face = "bold", size = 16)
 )
 
+##### predictors
+binned_subset_2 %>%
+  na.omit() %>% 
+  select(latitude, longitude, sst_raw_mean, sst_raw_var, sst_env_col, sst_bounded_seasonality) %>%
+  cor(x = ., method = c("spearman")) %>%
+  corrplot(method = "number")
+
+##### response
+binned_subset_2 %>%
+  na.omit() %>% 
+  select(bodysize_cwm_total, bodysize_cwv_total, sp_richness, shannon, total_biomass) %>%
+  cor(x = ., method = c("spearman")) %>%
+  corrplot(method = "number")
 
 ##### models
+binned_subset_log = binned_subset_2 %>% 
+  mutate(
+    number_total_log = log(number_total),
+    bodysize_cwm_total_log = log(bodysize_cwm_total),
+    bodysize_cwv_total_log = log(bodysize_cwv_total),
+    total_biomass_log = log(total_biomass),
+    sst_raw_mean_log = log(sst_raw_mean),
+    sst_raw_var_log = log(sst_raw_var),
+    sst_bounded_seasonality_log = log(sst_bounded_seasonality),
+    sst_colwell_p_log = log(sst_colwell_p)
+  ) %>% 
+  select(-c("number_total", "bodysize_cwm_total", "bodysize_cwv_total",
+            "total_biomass", "sst_raw_mean", "sst_raw_var", "sst_bounded_seasonality", "sst_colwell_p"))
 
 # bodysize
-summary(lm(binned_subset_2$bodysize_cwm_total ~ binned_subset_2$sst_raw_mean))
-summary(lm(binned_subset_2$bodysize_cwm_total ~ binned_subset_2$sst_raw_var))
-summary(lm(binned_subset_2$bodysize_cwm_total ~ binned_subset_2$sst_env_col))
-summary(lm(binned_subset_2$bodysize_cwm_total ~ binned_subset_2$sst_bounded_seasonality))
-summary(lm(binned_subset_2$bodysize_cwm_total ~ binned_subset_2$sst_colwell_p))
-summary(lm(binned_subset_2$bodysize_cwm_total ~ binned_subset_2$sst_raw_mean*binned_subset_2$sst_env_col))
-summary(lm(binned_subset_2$bodysize_cwm_total ~ binned_subset_2$sst_raw_mean*binned_subset_2$sst_bounded_seasonality))
-summary(lm(binned_subset_2$bodysize_cwm_total ~ 
-             binned_subset_2$sst_raw_mean*binned_subset_2$sst_env_col*binned_subset_2$sst_bounded_seasonality))
+cwm_models <- list(
+  cwm1 = lm(bodysize_cwm_total ~ sst_raw_mean, data = binned_subset_2),
+  cwm2 = lm(bodysize_cwm_total ~ sst_raw_var, data = binned_subset_2),
+  cwm3 = lm(bodysize_cwm_total ~ sst_raw_mean * sst_raw_var, data = binned_subset_2),
+  cwm4 = lm(bodysize_cwm_total ~ sst_env_col, data = binned_subset_2),
+  cwm5 = lm(bodysize_cwm_total ~ sst_bounded_seasonality, data = binned_subset_2),
+  cwm6 = lm(bodysize_cwm_total ~ sst_raw_mean * sst_env_col, data = binned_subset_2),
+  cwm7 = lm(bodysize_cwm_total ~ sst_raw_mean * sst_bounded_seasonality, data = binned_subset_2),
+  cwm8 = lm(bodysize_cwm_total ~ sst_raw_mean * sst_raw_var +
+              sst_raw_mean * sst_env_col * sst_bounded_seasonality, data = binned_subset_2),
+  cwm9 = lm(bodysize_cwm_total ~ sst_raw_mean * sst_raw_var +
+              sst_raw_var * sst_env_col * sst_bounded_seasonality, data = binned_subset_2),
+  cwm10 = lm(bodysize_cwm_total ~ sst_raw_mean * sst_raw_var * sst_env_col * sst_bounded_seasonality, data = binned_subset_2),
+  cwm11 = lm(bodysize_cwm_total ~ sst_raw_mean * sst_env_col * sst_bounded_seasonality, data = binned_subset_2)
+)
+
+qqp(cwm_models$cwm11)
+
+summary(lm(bodysize_cwm_total ~ 
+             sst_raw_mean * sst_env_col +
+             sst_raw_mean * sst_bounded_seasonality +
+             sst_env_col * sst_bounded_seasonality, 
+           data = binned_subset_2))
+
+step(cwm_models$cwm10)
+
+##### dredge 
+null_lm = lm(bodysize_cwm_total ~ 1, data = binned_subset_2)
+null_lm_2 = lm(bodysize_cwm_total ~ sst_raw_mean, data = binned_subset_2)
+null_lm_3 = lm(bodysize_cwm_total ~ sst_raw_mean * sst_raw_var, data = binned_subset_2)
+dredge_lm = lm(formula = bodysize_cwm_total ~ sst_raw_mean + sst_raw_var + 
+                 sst_env_col + sst_bounded_seasonality + sst_raw_var:sst_env_col + 
+                 sst_raw_var:sst_bounded_seasonality + sst_env_col:sst_bounded_seasonality + 
+                 sst_raw_var:sst_env_col:sst_bounded_seasonality, data = binned_subset_2)
+
+summary(null_lm)
+summary(null_lm_2)
+summary(null_lm_3)
+summary(dredge_lm)
+
+AIC(null_lm)
+AIC(null_lm_2)
+AIC(null_lm_3)
+AIC(dredge_lm)
+# aic wird niedriger obwohl mehr Variablen, niedriger AIc ist gut
+library(stats)
+anova(null_lm_3, dredge_lm)
+
+### scientific model: what effect do env col and seasonality have on body size?
+
+envcol_lm = lm(bodysize_cwm_total ~ sst_env_col, data = binned_subset_2)
+summary(envcol_lm) # ß coefficent = sst_env_col: 6.800 , not really interpretable
+
+# standardised version
+envcol_lm_2 = lm(scale(bodysize_cwm_total) ~ scale(sst_env_col), data = binned_subset_2)
+summary(envcol_lm_2) # no general effect of env col on body size
+#--> we have to include mean, ie, high means/ low means differently affect the effect of env col
+envcol_lm_3 = lm(scale(bodysize_cwm_total) ~ scale(sst_env_col) + scale(sst_raw_mean), data = binned_subset_2)
+summary(envcol_lm_3)
+
+envcol_lm_4 = lm(scale(bodysize_cwm_total) ~ scale(sst_bounded_seasonality) * scale(sst_raw_mean), data = binned_subset_2)
+summary(envcol_lm_4) # je höher der mean, desto steiler bodysize über seasonality
+# contextabhängige effekt von seasonality: scale(sst_bounded_seasonality):scale(sst_raw_mean)  0.16594
+
+# compare multiple models
+model.comparison = rbind(broom::glance(cwm_models$cwm1),
+                         broom::glance(cwm_models$cwm2),
+                         broom::glance(cwm_models$cwm3),
+                         broom::glance(cwm_models$cwm4),
+                         broom::glance(cwm_models$cwm5),
+                         broom::glance(cwm_models$cwm6),
+                         broom::glance(cwm_models$cwm7),
+                         broom::glance(cwm_models$cwm8),
+                         broom::glance(cwm_models$cwm9),
+                         broom::glance(cwm_models$cwm10),
+                         broom::glance(cwm_models$cwm11)
+                         )
+
+# bodysize variance
+cwv_models <- list(
+  cwv1 = lm(bodysize_cwv_total ~ sst_raw_mean, data = binned_subset_2),
+  cwv2 = lm(bodysize_cwv_total ~ sst_raw_var, data = binned_subset_2),
+  cwv3 = lm(bodysize_cwv_total ~ sst_raw_mean * sst_raw_var, data = binned_subset_2),
+  cwv4 = lm(bodysize_cwv_total ~ sst_env_col, data = binned_subset_2),
+  cwv5 = lm(bodysize_cwv_total ~ sst_bounded_seasonality, data = binned_subset_2),
+  cwv6 = lm(bodysize_cwv_total ~ sst_raw_var + sst_env_col, data = binned_subset_2),
+  cwv7 = lm(bodysize_cwv_total ~ sst_raw_var + sst_bounded_seasonality, data = binned_subset_2),
+  cwv8 = lm(bodysize_cwv_total ~ sst_raw_mean * sst_raw_var +
+              sst_raw_mean * sst_env_col * sst_bounded_seasonality, data = binned_subset_2),
+  cwv9 = lm(bodysize_cwv_total ~ sst_raw_mean * sst_raw_var +
+              sst_raw_var * sst_env_col * sst_bounded_seasonality, data = binned_subset_2),
+  cwv10 = lm(bodysize_cwv_total ~ sst_raw_mean * sst_raw_var * sst_env_col * sst_bounded_seasonality, data = binned_subset_2)
+)
+
+# compare multiple models
+model.comparison.2 = rbind(broom::glance(cwv_models$cwv1),
+                         broom::glance(cwv_models$cwv2),
+                         broom::glance(cwv_models$cwv3),
+                         broom::glance(cwv_models$cwv4),
+                         broom::glance(cwv_models$cwv5),
+                         broom::glance(cwv_models$cwv6),
+                         broom::glance(cwv_models$cwv7),
+                         broom::glance(cwv_models$cwv8),
+                         broom::glance(cwv_models$cwv9),
+                         broom::glance(cwv_models$cwv10)
+)
 
 # richness
-summary(lm(binned_subset_2$sp_richness ~ binned_subset_2$bodysize_cwm_total))
-summary(lm(binned_subset_2$sp_richness ~ binned_subset_2$sst_raw_mean))
-summary(lm(binned_subset_2$sp_richness ~ binned_subset_2$sst_env_col))
-summary(lm(binned_subset_2$sp_richness ~ binned_subset_2$sst_bounded_seasonality))
-summary(lm(binned_subset_2$sp_richness ~ binned_subset_2$sst_colwell_p))
-summary(lm(binned_subset_2$sp_richness ~ binned_subset_2$sst_raw_mean*binned_subset_2$sst_env_col))
-summary(lm(binned_subset_2$sp_richness ~ binned_subset_2$sst_raw_mean*binned_subset_2$sst_bounded_seasonality))
-summary(lm(binned_subset_2$sp_richness ~ 
-             binned_subset_2$sst_raw_mean*binned_subset_2$sst_env_col*binned_subset_2$sst_bounded_seasonality))
+rich_models <- list(
+  rich1 = lm(sp_richness ~ sst_raw_mean, data = binned_subset_2),
+  rich2 = lm(sp_richness ~ sst_raw_var, data = binned_subset_2),
+  rich3 = lm(sp_richness ~ sst_raw_mean * sst_raw_var, data = binned_subset_2),
+  rich4 = lm(sp_richness ~ sst_env_col, data = binned_subset_2),
+  rich5 = lm(sp_richness ~ sst_bounded_seasonality, data = binned_subset_2),
+  rich6 = lm(sp_richness ~ sst_raw_var + sst_env_col, data = binned_subset_2),
+  rich7 = lm(sp_richness ~ sst_raw_var + sst_bounded_seasonality, data = binned_subset_2),
+  rich8 = lm(sp_richness ~ sst_raw_mean * sst_raw_var +
+              sst_raw_mean * sst_env_col * sst_bounded_seasonality, data = binned_subset_2),
+  rich9 = lm(sp_richness ~ sst_raw_mean * sst_raw_var +
+              sst_raw_var * sst_env_col * sst_bounded_seasonality, data = binned_subset_2),
+  rich10 = lm(sp_richness ~ sst_raw_mean * sst_raw_var * sst_env_col * sst_bounded_seasonality, data = binned_subset_2)
+)
 
-# shannon
-summary(lm(binned_subset_2$total_biomass ~ binned_subset_2$sst_raw_mean))
-summary(lm(binned_subset_2$total_biomass ~ binned_subset_2$sst_raw_var))
-summary(lm(binned_subset_2$total_biomass ~ binned_subset_2$sst_env_col))
-summary(lm(binned_subset_2$total_biomass ~ binned_subset_2$sst_bounded_seasonality))
-summary(lm(binned_subset_2$total_biomass ~ binned_subset_2$sst_colwell_p))
-summary(lm(binned_subset_2$total_biomass ~ binned_subset_2$sst_raw_mean*binned_subset_2$sst_env_col))
-summary(lm(binned_subset_2$total_biomass ~ binned_subset_2$sst_raw_mean*binned_subset_2$sst_bounded_seasonality))
-summary(lm(binned_subset_2$total_biomass ~ 
-             binned_subset_2$sst_raw_mean*binned_subset_2$sst_env_col*binned_subset_2$sst_bounded_seasonality))
+# compare multiple models
+model.comparison.3 = rbind(broom::glance(rich_models$rich1),
+                         broom::glance(rich_models$rich2),
+                         broom::glance(rich_models$rich3),
+                         broom::glance(rich_models$rich4),
+                         broom::glance(rich_models$rich5),
+                         broom::glance(rich_models$rich6),
+                         broom::glance(rich_models$rich7),
+                         broom::glance(rich_models$rich8),
+                         broom::glance(rich_models$rich9),
+                         broom::glance(rich_models$rich10)
+)
